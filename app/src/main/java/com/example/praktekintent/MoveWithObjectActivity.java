4ec1eac91293e389c8dca010c4a305e9b868626c
atika1004 <istiqomahatika.ai@gmail.com>
2020-10-12 10:31:04 +0700

    Modul2-Intent

---
+++ app/src/main/java/com/example/praktekintent/MoveWithObjectActivity.java
@@ -0,0 +1,22 @@
+package com.example.praktekintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MoveWithObjectActivity extends AppCompatActivity {
+    public static String EXTRA_PERSON = "extra_person";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_with_object);
+        TextView tvObject = this.findViewById(R.id.tv_object_received);
+        Person mPerson = getIntent().getParcelableExtra(EXTRA_PERSON);
+        assert mPerson != null;
+        String text = "Name : "+ mPerson.getName() +", Email : "+ mPerson.getEmail()
+                        +", Age : "+ mPerson.getAge() +", Location : "+ mPerson.getCity();
+        tvObject.setText(text);
+    }
+}
